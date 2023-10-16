824832aadee66fcc72d0a5f4947ee65c8cc7dd0b
parth-451D <parth.dalsaniya2001@gmail.com0>
2023-10-16 22:41:51 +0530

    second

--- src/components/PaperPage.js
+++ src/components/PaperPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Checkbox from "./common/Checkbox";
 
 const PaperPage = () => {
+  const [questionList, setQuestionList] = useState([]);
+  const getQuestionListData = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/questions");
+      setQuestionList(res.data);
+      console.log("gsddssgdgss", res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getQuestionListData();
+  }, []);
   return (
     <div>
-        
-    </div>
-  )
-}
+      fdgszvsdgsd
+      {questionList &&
+  questionList.map((ele, idx) => {
+    console.log("1", ele);
+    if (ele.questionType === "Checkbox") {
+      console.log("2");
+      return <Checkbox optionList={ele.questionOptions} key={idx} />;
+    }
+    return null; // Make sure to return something from map
+  })}
 
-export default PaperPage
+    </div>
+  );
+};
+
+export default PaperPage;
--- src/components/common/Checkbox.js
+++ src/components/common/Checkbox.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 
 const Checkbox = (props) => {
-  const [answer, setAnswer] = useState();
+  console.log("object");
+  const [answer, setAnswer] = useState([]);
   const [shuffledOptions, setShuffledOptions] = useState([]);
-  
+
   useEffect(() => {
     // Shuffle the options array when it changes
     const shuffled = props.optionList.sort(() => Math.random() - 0.5);
@@ -22,6 +23,27 @@ const Checkbox = (props) => {
       setAnswer((prevAnswer) => prevAnswer.filter((item, i) => i !== index));
     }
   };
+
+  const handleCheckChange = (e, ele) => {
+    const oldArr = localStorage.getItem("answerList")
+    if (answer && answer.includes(ele)) {
+      answer.splice(e.target.checked, 1);
+    } else {
+      answer.push(e.target.checked);
+    }
+    const index = oldArr?.findIndex((item) => item.id === props._id);
+    if (index !== -1) {
+        // If the object with the id exists, update the ans property
+        oldArr[index].answer = answer;
+      } else {
+        // If the object with the id doesn't exist, add a new object
+        oldArr.push({ id: props._id, answer: answer });
+      }
+    localStorage.setItem("answerList", oldArr)
+  };
+
+  console.log(localStorage.getItem("answerList"))
+
   return (
     <div>
       {props &&
@@ -32,8 +54,8 @@ const Checkbox = (props) => {
               type="checkbox"
               id="checkbox"
               label={ele}
-              checked={answer.includes(shuffledOptions[idx])}
-              onChange={(event) => handleChange(event, idx)}
+              checked={answer && answer.includes(answer[idx])}
+              onChange={(event) => handleCheckChange(event, ele)}
             />
           );
         })}
